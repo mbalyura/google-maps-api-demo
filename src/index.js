0f1139bd1a98db552d7e978b9610216110a231d3
mbalyura <m.balyura@gmail.com>
2021-02-04 14:25:08 +0300

    remove simplebar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import 'regenerator-runtime/runtime';
-import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
-import 'simplebar/dist/simplebar.css';
 import './scss/app.scss';
 import './images/favicon.png';
 
